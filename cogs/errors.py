39f2af1f433978ad6a6c475916415e05092a9c93
Olivia Palmu <oliviaspalmu@gmail.com>
2020-09-22 11:25:17 +0900

    Add a simple error handler

--- cogs/errors.py
+++ cogs/errors.py
@@ -9,5 +9,20 @@ class Errors(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        error = getattr(error, "original", error)
+        
+        ignored = (commands.CommandNotFound, commands.NotOwner)
+        if isinstance(error, ignored):
+            return
+
+        embed = discord.Embed(
+            title=error.__class__.__name__,
+            description=str(error),
+            color=ctx.color,
+        )
+        await ctx.boom(embed=embed)
+
 def setup(bot):
     bot.add_cog(Errors(bot))
